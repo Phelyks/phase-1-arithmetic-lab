52404461e293245fe2faea6c063790f392508daa
Felix <flexxmosh@gmail.com>
2022-08-21 11:02:19 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 2;
+let num2 = 31;
+
+const multiply = num1 * num2;
+
 
 // Write your code here
+
+//random
+
+let random = Math.floor(Math.random() * 88);
+
+
+// modules
+
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+let max = Math.max(8, 20, 5, 7.5, 8.9,);
